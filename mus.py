4862784fed464ee14c25c27b9811a45fee450614
Dhruva <dkarkada@gmail.com>
2018-03-30 04:38:35 -0500

    starting to add some functionality

--- mus.py
+++ mus.py
@@ -1,8 +1,37 @@
 import sys
+import re
+from datetime import date
+
+class Artist:
+	def __init__(self, name):
+		self.name = name
+		self.songs = []
+	def getSongs(self):
+		return self.songs
+
+class Song:
+	def __init__(self, title, month, year, ind, pl):
+		self.title = title
+		self.artists = []
+		self.date = date(year, month, 1)
+		self.index = ind
+		self.playlist = pl
+
+def parse(quiet, artists, playlists):
+	file = open('data.txt', 'r')
+	for line in file:
+		songdata = re.split(r"\t", line)
+		
+	file.close()	
 
 def main():
 	quiet = len(sys.argv)>0 and sys.argv[1].lower()=='true'
-	if (not quiet): print("Starting mus...")
+	if (not quiet):
+		print("Starting mus...")
+		print("Reading data...")
+	artists = {}
+	playlists = {}
+	parse(quiet, artists, playlists)	
 	done = False
 	while (not done):
 		try:
