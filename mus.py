00301d4e259034cfbeb5caf303c77de57a569246
Dhruva <dkarkada@gmail.com>
2018-03-31 04:51:58 -0500

    starting to work on command line interface

--- mus.py
+++ mus.py
@@ -47,12 +47,14 @@ def parse(quiet, artists, playlists):
 			if playlist not in playlists:
 				playlists[playlist] = []
 			else:
-				err = "Playlist double specified:"
-				print("{} {}".format(err, line))
+				if not quiet:
+					err = "Playlist double specified:"
+					print("{} {}".format(err, line))
 			for info in songdata[1:]:
 				if (info.strip() != ""):
-					err = "Playlist should be specified in its own line:"
-					print("{} {}".format(err, line))
+					if not quiet:
+						err = "Playlist should be specified in its own line:"
+						print("{} {}".format(err, line))
 		else:
 			if songdata[1].strip() != "":
 				month, year = parseDate(songdata[1].strip())
@@ -60,20 +62,23 @@ def parse(quiet, artists, playlists):
 				ind += 1
 				title = songdata[2].strip()
 				song = Song(title, month, year, ind, playlist)
-				# expectFlag = False
 				ppl = [info.strip()[1:] if info.strip()[0]=="\\" else info.strip()\
-						for info in songdata[3:] if len(info.strip())>1 and info.strip()[0]!=":"]
-				artistmeta = [info.strip()[1:] for info in songdata[3:] if len(info.strip())>0 and info.strip()[0]==":"]
+						for info in songdata[3:]\
+						if len(info.strip())>1 and info.strip()[0]!=":"]
+				artistmeta = [info.strip()[1:] for info in songdata[3:]\
+								if len(info.strip())>0 and info.strip()[0]==":"]
 				if (len(artistmeta)==0):
 					artistmeta.append("o")
 				elif (len(artistmeta)>1):
-					err = "Multiple instances of artist metainfo:"
-					print("{} {}".format(err, line))
+					if not quiet:
+						err = "Multiple instances of artist metainfo:"
+						ifprint("{} {}".format(err, line))
 				for i in range(len(ppl)):
 					meta = artistmeta[0]
 					if (i >= len(meta)):
-						err = "Artist metainfo has insufficient length:"
-						print("{} {}".format(err, line))
+						if not quiet:
+							err = "Artist metainfo has insufficient length:"
+							print("{} {}".format(err, line))
 						role = 'o'
 					else:
 						role = meta[i]
@@ -119,13 +124,31 @@ def main():
 		except KeyboardInterrupt:
 			print()
 			sys.exit();
-		args = inp.split(" ")
+		args = re.split(r"(?<!\\)\s+", inp)
+		args = [re.sub(r"(\\) "," ", arg).lower() for arg in args]
 		if (len(args) != 0):
 			cmd = args[0]
 			if (cmd in ('quit','exit')):
 				done = True
-			elif (cmd == 'ls'):
-				print("AA")
-
+			elif (cmd == 'disc'):
+				if (len(args)==1 and not quiet):
+					print("disc command requires argument(s)")
+				else:
+					arg = " ".join(args[1:]).lower()
+					if arg in artists:
+						artist = artists[arg]
+					else:
+						matches = [name for name in artists if arg in name]
+						if len(matches)==1:
+							artist = artists[matches[0]]
+						else:
+							
+					print("-"*32)
+					print(" "*((32-len(artist.name))//2) + artist.name)
+					print("-"*32)
+					for title in artist.songs:
+						song, role = artist.songs[title]
+						print("{} {}".format(role, song.title))
+					print()
 if __name__ == "__main__":
 	main()
